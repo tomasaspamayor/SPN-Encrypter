f2cc903530640dad030e6520fa55be65f7984a10
Louis Liu <liulouis1@gmail.com>
2026-03-10 15:11:33 +0000

    main workflow --not including key mixing yet

--- main.s
+++ main.s
@@ -2,38 +2,72 @@
 
 global  pkg_buffer
 extrn   UART_Setup, UART_Receive_Package, UART_Send_Package
-extrn	SBOX_Encrypt_Byte, Encrypt_Buffer, SBOX_Decrypt_Byte, Decrypt_Buffer
-extrn	Key_Setup, Mix_Key
+extrn	SBOX_Encrypt_Byte, SBOX_Encrypt_Buffer, SBOX_Decrypt_Byte, SBOX_Decrypt_Buffer
+extrn	Key_Setup
+extrn   P_Box_Enc, P_Box_Dec
 
-; --- Reserve 16 bytes for the 128-bit packet ---
 psect  udata_acs
 pkg_buffer:  ds 16
-CLEAR_CNT:   ds 1          ; NEED THIS: counter variable for clearing buffer
+CLEAR_CNT:   ds 1          ; Counter for clearing buffer
+n_cycles:    ds 1          ; Counter for number of encryption cycles
 
-; --- Code section ---
+
 psect   code
 Setup:
-    call    UART_Setup          ; Initialize UART
-    call    Key_Setup		; generate key
+        call    UART_Setup          ; Initialize UART
+        call    Key_Setup		    ; generate key
+        ; generate keys and start scheduling
+
+Encrypt:
+        movlw  10                  ; Number of encryption cycles
+        movwf  n_cycles, A         ; Store in cycle counter variable
+
+        ; mix first key
+
+    Encrypt_Loop:
+        call    SBOX_Encrypt_Buffer
+        call    P_Box_Enc
+
+        ; mix all other keys
+
+        decfsz  n_cycles, F, A         ; Decrement cycle counter, skip if zero
+        bra     Encrypt_Loop
+
+
+Decrypt: 
+        movlw   10                   ; Number of decryption cycles
+        movwf   n_cycles, A          ; Store in cycle counter variable
+
+        ; mix first key
+
+    Decrypt_Loop:
+        call    P_Box_Dec
+        call    SBOX_Decrypt_Buffer
+
+        ; mix all other keys
+
+        decfsz  n_cycles, F, A         ; Decrement cycle counter, skip if zero
+        bra     Decrypt_Loop
+
 Loop:
-    ; --- Step 0: Clear pkg_buffer to avoid leftover RAM content ---
-    lfsr    2, pkg_buffer       ; FSR2 points to start of buffer
-    movlw   16                  ; Number of bytes to clear
-    movwf   CLEAR_CNT, A        ; Store in counter variable
-    movlw   0                   ; Load 0 into WREG for clearing
+        lfsr    2, pkg_buffer       ; FSR2 points to start of buffer
+        movlw   16                  ; Number of bytes to clear
+        movwf   CLEAR_CNT, A        ; Store in counter variable
+        movlw   0                   ; Load 0 into WREG for clearing
+
 Clear_Loop:
-    movwf   POSTINC2, A            ; Write W=0 (clear) and increment pointer
-    decfsz  WREG, F, A             ; Decrement counter, skip if zero
-    bra     Clear_Loop
+        movwf   POSTINC2, A            ; Write W=0 (clear) and increment pointer
+        decfsz  WREG, F, A             ; Decrement counter, skip if zero
+        bra     Clear_Loop
 
-    ; --- Step 1: Receive 16-byte packet from PC ---
-    call    UART_Receive_Package
+        call    UART_Receive_Package
 
-    ; --- Step 2: Process the buffer (optional encryption/decryption) ---
-    ; [Insert your algorithm here, operating on pkg_buffer]
-    call    Decrypt_Buffer
+        ; either encryopt or decrypt the package based on some condition 
 
-    ; --- Step 3: Send the 16-byte packet back to PC ---
-    call    UART_Send_Package
+        call    Encrypt
 
-    bra     Loop                ; Repeat indefinitely
+        call    Decrypt
+
+        call    UART_Send_Package
+
+        bra     Loop                ; Repeat indefinitely
