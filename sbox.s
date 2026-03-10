96d3c5ddb2580f810193f75b090ffd5c884a84b2
Louis Liu <liulouis1@gmail.com>
2026-03-10 15:11:59 +0000

    renaming sbox encryption pathways

--- sbox.s
+++ sbox.s
@@ -4,7 +4,7 @@
 ; We will use the Rinjael S-Box, which is a well-known S-Box used in the AES encryption standard.
 ; There will be a symmetrical file to decrypt the message, which will use the inverse of the Rinjael S-Box.
 
-global  SBOX_Encrypt_Byte, Sbox_Encrypt_Buffer, SBOX_Decrypt_Byte, Sbox_Decrypt_Buffer
+global  SBOX_Encrypt_Byte, SBOX_Encrypt_Buffer, SBOX_Decrypt_Byte, SBOX_Decrypt_Buffer
 extrn   pkg_buffer
 
 ; Define your custom registers in the variable section
@@ -201,7 +201,7 @@ SBOX_Encrypt_Byte:
 ; Output: pkg_buffer - 16 bytes of ciphertext (overwritten)
 ; Destroys: WREG, TBLPTR, TABLAT, FSR1, COUNT, AL, TEMP
 ;----------------------------------------------------------
-Sbox_Encrypt_Buffer:
+SBOX_Encrypt_Buffer:
     ; Set up FSR1 to point to pkg_buffer
     movlw   LOW(pkg_buffer) ; Get low byte of buffer address
     movwf   FSR1L, A        ; Store in FSR1 low
@@ -273,7 +273,7 @@ SBOX_Decrypt_Byte:
 ; Output: pkg_buffer - 16 bytes of plaintext (overwritten)
 ; Destroys: WREG, TBLPTR, TABLAT, FSR1, COUNT, AL, TEMP
 ;----------------------------------------------------------
-Sbox_Decrypt_Buffer:
+SBOX_Decrypt_Buffer:
     ; Set up FSR1 to point to pkg_buffer
     movlw   LOW(pkg_buffer) ; Get low byte of buffer address
     movwf   FSR1L, A        ; Store in FSR1 low
